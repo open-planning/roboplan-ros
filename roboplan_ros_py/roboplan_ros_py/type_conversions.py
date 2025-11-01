0bd468ebbd4ef19bde18d76a1e754a5604b88bcb
Erik Holum <eholum@gmail.com>
2025-11-01 10:43:31 -0400

    Joint state conversions

--- roboplan_ros_py/roboplan_ros_py/type_conversions.py
+++ roboplan_ros_py/roboplan_ros_py/type_conversions.py
@@ -2,9 +2,9 @@ import numpy as np
 import pinocchio as pin
 
 from builtin_interfaces.msg import Duration
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Pose, TransformStamped
+from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from geometry_msgs.msg import Pose
 
 import roboplan
 
@@ -36,6 +36,47 @@ def from_duration(duration: Duration) -> float:
         An equivalent double timestamp.
     """
     return duration.sec + duration.nanosec * 1e-9
+
+
+def to_joint_state(joint_configuration: roboplan.JointConfiguration) -> JointState:
+    """
+    Converts a roboplan.JointConfiguration object to a ROS 2 JointState message.
+
+    Args:
+        joint_configuration: The roboplan JointConfiguration to convert.
+
+    Returns:
+        An equivalent ROS 2 JointState message
+    """
+    joint_state = JointState()
+    joint_state.name = joint_configuration.joint_names
+    joint_state.position = joint_configuration.positions.tolist()
+    joint_state.velocity = joint_configuration.velocities.tolist()
+
+    # Effort != acceleration but including and kind of abusing the interface.
+    joint_state.effort = joint_configuration.accelerations.tolist()
+    return joint_state
+
+
+def from_joint_state(joint_state: JointState):
+    """
+    Convert the provided ROS 2 JointState message to an equivalent
+    roboplan.JointConfiguration.
+
+    Args:
+        joint_state: The ROS 2 JointState message to convert.
+
+    Returns:
+        An equivalent roboplan.JointConfiguration.
+    """
+    joint_configuration = roboplan.JointConfiguration()
+    joint_configuration.joint_names = joint_state.name
+    joint_configuration.positions = np.array(joint_state.position)
+    joint_configuration.velocities = np.array(joint_state.velocity)
+
+    # Effort != acceleration but including and kind of abusing the interface.
+    joint_configuration.accelerations = np.array(joint_state.effort)
+    return joint_configuration
 
 
 def to_joint_trajectory(
