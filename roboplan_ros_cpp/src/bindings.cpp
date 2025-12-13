f90ae85848dc6cac5630b92aa00aea12997507c1
Erik Holum <eholum@gmail.com>
2025-12-13 12:58:18 -0500

    Use exported roboplan bindings interface

--- roboplan_ros_cpp/src/bindings.cpp
+++ roboplan_ros_cpp/src/bindings.cpp
@@ -7,39 +7,14 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
 
+#include <utils/expected.hpp>
+
 #include "roboplan_ros_cpp/bindings.hpp"
 #include "roboplan_ros_cpp/type_conversions.hpp"
 
 namespace nb = nanobind;
 using namespace nb::literals;
 using namespace roboplan_ros_cpp;
-
-namespace {
-
-/// @brief Helper function for checking if a tl::expected return type has a value or an error.
-/// @details If a value exists, we return it as is. When handling unexpected values, if
-/// the error is string convertible then we can pass it along through a `std::runtime_error`.
-/// Otherwise we do not know the details of the underlying exception.
-/// @return The unwrapped value, or throw an exception.
-/// @throw std::runtime_error if the result is an error.
-template <typename Expected> typename Expected::value_type handle_expected(Expected&& result) {
-  if (result.has_value()) {
-    if constexpr (std::is_void_v<typename Expected::value_type>) {
-      return;  // Return nothing since it's a void type
-    } else {
-      return result.value();
-    }
-  } else {
-    // TODO: Consider wrapping with a streamable option.
-    if constexpr (std::is_convertible_v<typename Expected::error_type, std::string>) {
-      throw std::runtime_error(std::string(result.error()));
-    } else {
-      throw std::runtime_error("Unknown error occurred.");
-    }
-  }
-};
-
-}  // namespace
 
 NB_MODULE(bindings, m) {
   m.doc() = "RoboPlan ROS Python bindings for C++ Type Conversions";
